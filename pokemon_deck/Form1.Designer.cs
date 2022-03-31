dd1b5a24d09f1008f82004a9f063e1dbc700e7e3
mbbra567 <75265256+mbbra567@users.noreply.github.com>
2022-03-31 08:54:02 -0700

    Add files via upload

---
+++ pokemon_deck/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace pokemon_deck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.WeighttextBox = new System.Windows.Forms.TextBox();
+            this.HeighttextBox = new System.Windows.Forms.TextBox();
+            this.TypetextBox = new System.Windows.Forms.TextBox();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Prevbutton = new System.Windows.Forms.Button();
+            this.Nextbutton = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Lastbutton = new System.Windows.Forms.Button();
+            this.Savebutton = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MovestextBox = new System.Windows.Forms.TextBox();
+            this.HptextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(67, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 302);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(67, 350);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(369, 88);
+            this.textBox1.TabIndex = 1;
+            // 
+            // WeighttextBox
+            // 
+            this.WeighttextBox.Location = new System.Drawing.Point(527, 148);
+            this.WeighttextBox.Name = "WeighttextBox";
+            this.WeighttextBox.Size = new System.Drawing.Size(100, 20);
+            this.WeighttextBox.TabIndex = 2;
+            // 
+            // HeighttextBox
+            // 
+            this.HeighttextBox.Location = new System.Drawing.Point(527, 122);
+            this.HeighttextBox.Name = "HeighttextBox";
+            this.HeighttextBox.Size = new System.Drawing.Size(100, 20);
+            this.HeighttextBox.TabIndex = 3;
+            // 
+            // TypetextBox
+            // 
+            this.TypetextBox.Location = new System.Drawing.Point(527, 96);
+            this.TypetextBox.Name = "TypetextBox";
+            this.TypetextBox.Size = new System.Drawing.Size(100, 20);
+            this.TypetextBox.TabIndex = 4;
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(527, 70);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(100, 20);
+            this.NametextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(468, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(468, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Type:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Height:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(468, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Weight:";
+            // 
+            // Prevbutton
+            // 
+            this.Prevbutton.Location = new System.Drawing.Point(488, 357);
+            this.Prevbutton.Name = "Prevbutton";
+            this.Prevbutton.Size = new System.Drawing.Size(75, 23);
+            this.Prevbutton.TabIndex = 12;
+            this.Prevbutton.Text = "<";
+            this.Prevbutton.UseVisualStyleBackColor = true;
+            this.Prevbutton.Click += new System.EventHandler(this.Prevbutton_Click);
+            // 
+            // Nextbutton
+            // 
+            this.Nextbutton.Location = new System.Drawing.Point(569, 357);
+            this.Nextbutton.Name = "Nextbutton";
+            this.Nextbutton.Size = new System.Drawing.Size(75, 23);
+            this.Nextbutton.TabIndex = 13;
+            this.Nextbutton.Text = ">";
+            this.Nextbutton.UseVisualStyleBackColor = true;
+            this.Nextbutton.Click += new System.EventHandler(this.Nextbutton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(488, 386);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "First";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Lastbutton
+            // 
+            this.Lastbutton.Location = new System.Drawing.Point(569, 386);
+            this.Lastbutton.Name = "Lastbutton";
+            this.Lastbutton.Size = new System.Drawing.Size(75, 23);
+            this.Lastbutton.TabIndex = 15;
+            this.Lastbutton.Text = "Last";
+            this.Lastbutton.UseVisualStyleBackColor = true;
+            this.Lastbutton.Click += new System.EventHandler(this.Lastbutton_Click);
+            // 
+            // Savebutton
+            // 
+            this.Savebutton.Location = new System.Drawing.Point(527, 415);
+            this.Savebutton.Name = "Savebutton";
+            this.Savebutton.Size = new System.Drawing.Size(75, 23);
+            this.Savebutton.TabIndex = 16;
+            this.Savebutton.Text = "Save";
+            this.Savebutton.UseVisualStyleBackColor = true;
+            this.Savebutton.Click += new System.EventHandler(this.Savebutton_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(468, 184);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Moves:";
+            // 
+            // MovestextBox
+            // 
+            this.MovestextBox.Location = new System.Drawing.Point(527, 174);
+            this.MovestextBox.Name = "MovestextBox";
+            this.MovestextBox.Size = new System.Drawing.Size(100, 20);
+            this.MovestextBox.TabIndex = 22;
+            // 
+            // HptextBox
+            // 
+            this.HptextBox.Location = new System.Drawing.Point(527, 200);
+            this.HptextBox.Name = "HptextBox";
+            this.HptextBox.Size = new System.Drawing.Size(100, 20);
+            this.HptextBox.TabIndex = 23;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(468, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 13);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Hp:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(64, 334);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 13);
+            this.label7.TabIndex = 25;
+            this.label7.Text = "Description";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.HptextBox);
+            this.Controls.Add(this.MovestextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.Savebutton);
+            this.Controls.Add(this.Lastbutton);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Nextbutton);
+            this.Controls.Add(this.Prevbutton);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.TypetextBox);
+            this.Controls.Add(this.HeighttextBox);
+            this.Controls.Add(this.WeighttextBox);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox WeighttextBox;
+        private System.Windows.Forms.TextBox HeighttextBox;
+        private System.Windows.Forms.TextBox TypetextBox;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button Prevbutton;
+        private System.Windows.Forms.Button Nextbutton;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Lastbutton;
+        private System.Windows.Forms.Button Savebutton;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox MovestextBox;
+        private System.Windows.Forms.TextBox HptextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ pokemon_deck/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Text.Json.Serialization;
+using static System.Console;
+using System.IO;
+namespace pokemon_deck
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList pokes = new ArrayList();
+        int current;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Savebutton_Click(object sender, EventArgs e)
+        {
+            var p = new Pokemon
+            {
+                name = NametextBox.Text,
+                Height = HeighttextBox.Text,
+                Type = TypetextBox.Text,
+                Weight = WeighttextBox.Text,
+                Moves = MovestextBox.Text,
+                HP = HptextBox.Text,
+                Description = textBox1.Text,
+                Picture = pictureBox1.ImageLocation
+            };
+            if(pictureBox1 == null) { MessageBox.Show("Select a picture"); }
+            string pokemonstring = JsonSerializer.Serialize(p);
+            pokes.Add(p);
+            StreamWriter outfile = File.CreateText("pokemon.json");
+            foreach (var item in pokes)
+            {
+                outfile.WriteLine(JsonSerializer.Serialize(item));
+            }
+            outfile.Close();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+
+            if (System.IO.File.Exists(openFileDialog1.FileName))
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
+        }
+        public void show()
+        {
+            Pokemon p = (Pokemon)pokes[current];
+            NametextBox.Text = p.name;
+            HeighttextBox.Text = p.Height;
+            TypetextBox.Text = p.Type;
+            WeighttextBox.Text = p.Weight;
+            HptextBox.Text = p.HP;
+            MovestextBox.Text = p.Moves;
+            textBox1.Text = p.Description;
+            if (File.Exists(p.Picture))
+                {
+                pictureBox1.Load(p.Picture);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader infile = File.OpenText("Pokemon.json");
+            while(infile.Peek() != -1)
+            {
+                string pstring = infile.ReadLine();
+                Pokemon p = JsonSerializer.Deserialize<Pokemon>(pstring);
+                pokes.Add(p);
+            }
+            infile.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            show();
+        }
+
+        private void Nextbutton_Click(object sender, EventArgs e)
+        {
+            if (pokes.Count == current + 1)
+            {
+                current = 0;
+            }
+            else if (current < pokes.Count)
+            {
+                current++;
+            }
+            Show();
+        }
+
+        private void Prevbutton_Click(object sender, EventArgs e)
+        {
+            if (current == 0)
+            {
+                current = pokes.Count - 1;
+            }
+            else if (current > 0)
+            {
+                current--;
+            }
+            Show();
+        }
+
+        private void Lastbutton_Click(object sender, EventArgs e)
+        {
+           current = pokes.Count - 1;
+           show();
+        }
+    }
+}
